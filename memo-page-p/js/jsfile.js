d8a175de82eee3d4be38921d8c06a3ae8e87b11e
s.zhu <wjzsh@DESKTOP-J5IH7SN.localdomain>
2020-07-26 15:59:56 +0200

    draft profolio page

---
+++ memo-page-p/js/jsfile.js
@@ -0,0 +1,40 @@
+function checkme(message)
+{
+  switch (message) {
+    case "Experience":
+      x=document.getElementById("Experience");
+      unfade(x);
+      break;
+    default:
+
+  }
+}
+
+
+
+
+
+function unfade(element) {
+    var op = 0.1;  // initial opacity
+    element.style.display = 'block';
+    var timer = setInterval(function () {
+        if (op >= 1){
+            clearInterval(timer);
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op += op * 0.1;
+    }, 10);
+}
+function fade(element) {
+    var op = 1;  // initial opacity
+    var timer = setInterval(function () {
+        if (op <= 0.1){
+            clearInterval(timer);
+            element.style.display = 'none';
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op -= op * 0.1;
+    }, 50);
+}
